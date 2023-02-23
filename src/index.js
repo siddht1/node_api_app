60eb1559d170c2856129d9a116c29fa2adbd5088
siddht1 <rt1yrru+github@gmail.com>
2023-02-23 20:23:15 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// In src/index.js
+const express = require("express");
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// *** REMOVE ***
+app.get("/", (req, res) => {
+  res.send("<h2>It's Working!</h2>");
+});
+
+
+app.listen(PORT, () => {
+  console.log(`API is listening on port ${PORT}`);
+});
