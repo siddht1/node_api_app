fef076e624f2f40a87b4e474d8335763a1118ff3
siddht1 <rt1yrru+github@gmail.com>
2023-09-25 23:21:54 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors()); // Enable CORS for all origins
-
 app.use(bodyParser.json({ limit: '1mb' }));
 app.use(bodyParser.urlencoded({ limit: '1mb', extended: true }));
 
+// Endpoint for the GET request
 app.get("/", (req, res) => {
   const data = {
     "GET": req.query
@@ -17,12 +17,23 @@ app.get("/", (req, res) => {
   res.send(data);
 });
 
+// Endpoint for the POST request
 app.post("/", (req, res) => {
   console.log("POST request received");
   const data = {
     "POST": req.body
   };
   res.send(data);
+});
+
+// Endpoint for the openai chat completions
+app.post("/api/openai/v1/chat/completions", (req, res) => {
+  console.log("POST request for openai chat completions received");
+  const users = [
+    { id: 1, name: 'Test_2' },
+    { id: 2, name: 'Test_2' }
+  ];
+  res.send(users);
 });
 
 app.listen(PORT, () => {
