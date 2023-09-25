7fc821b004ba6723d7fb5eb868b533edd35d9a2b
siddht1 <rt1yrru+github@gmail.com>
2023-09-26 01:01:59 +0530

    Update index.js

--- src/v1/routes/index.js
+++ src/v1/routes/index.js
@@ -1,26 +1,9 @@
+// In src/v1/routes/index.js
 const express = require("express");
 const router = express.Router();
-const cors = require('cors');
 
-// Enable CORS for all routes
-router.use(cors());
-
-// Custom middleware function to check user's unique code
-const checkUniqueCode = (req, res, next) => {
-  const { code } = req.query;
-  
-  // Check if the code is valid (you will need to implement this according to your requirements)
-  if (code === "myUniqueCode123") {
-    return next(); // Code is valid, proceed to the OpenAI endpoint
-  }
-  
-  // Code is invalid, return an error message
-  return res.status(401).json({ error: "Unauthorized access" });
-};
-
-// Route for OpenAI endpoint with custom middleware
-router.route("/openai").get(checkUniqueCode, (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
+router.route("/").get((req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
   res.setHeader('Access-Control-Allow-Origin', '*')
   // another common pattern
   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
@@ -29,9 +12,10 @@ router.route("/openai").get(checkUniqueCode, (req, res) => {
     'Access-Control-Allow-Headers',
     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
   )
-  
-  // Implement the logic to call the OpenAI endpoint here
+    var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
   res.send({"status":"OK","API":"WORKING @ V1","API_V1":"/API/V1","url":fullUrl,"CORS":"allowed"});
 });
 
 module.exports = router;
+
+
