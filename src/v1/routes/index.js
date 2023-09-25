4822b2efafe36be6782b405e774eb50e02af6880
siddht1 <rt1yrru+github@gmail.com>
2023-09-26 00:54:43 +0530

    Update index.js

--- src/v1/routes/index.js
+++ src/v1/routes/index.js
@@ -4,25 +4,18 @@ const cors = require('cors');
 
 // Enable CORS for all routes
 router.use(cors());
-app.get('/', async (req, res) => {
-  console.log('Received chat completions request');
-  const { prompt } = req.body;
-  
-  // Call OpenAI API to get completion
-  const completion = await getChatCompletion(prompt);
-  
-  // Return completion as JSON response
-  res.json({ completion });
-});
-app.post('/', async (req, res) => {
-  console.log('Received chat completions request');
-  const { prompt } = req.body;
-  
-  // Call OpenAI API to get completion
-  const completion = await getChatCompletion(prompt);
-  
-  // Return completion as JSON response
-  res.json({ completion });
+router.route("/").get((req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+    var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+  res.send({"status":"OK","API":"WORKING @ V1","API_V1":"/API/V1","url":fullUrl,"CORS":"allowed"});
 });
 
 module.exports = router;
