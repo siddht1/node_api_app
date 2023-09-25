2061920ba2b8d74fd454fe2d55a2cc7a79c690df
siddht1 <rt1yrru+github@gmail.com>
2023-09-26 01:50:57 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
-// Modified code
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const { createClient } = require('@supabase/supabase-js');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const v1Router = require("./v1/routes");
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 app.use(cors()); // Enable CORS for all origins
 app.use(bodyParser.json({ limit: '1mb' }));
 app.use(bodyParser.urlencoded({ limit: '1mb', extended: true }));
 
 // Endpoint for all requests
-app.use("*", (req, res) => {
+app.use("*", async (req, res) => {
   const data = {
     "status": "ok",
     "url": req.originalUrl,
@@ -20,7 +22,16 @@ app.use("*", (req, res) => {
     "request_body": req.body,
     "request_method": req.method
   };
-  res.send(data);
+  
+  const { data: log, error } = await supabase.from('db_test').insert(data); 
+
+  if (error) {
+    console.error('Error inserting log:', error);
+    res.status(500).send('Error inserting log');
+  } else {
+    console.log('Log inserted successfully:', log);
+    res.send(data);
+  }
 });
 
 // Use v1Router for version 1 API requests
@@ -34,3 +45,4 @@ app.listen(PORT, () => {
 // 1. Added request_method to data object to display the HTTP method used in the request.
 // 2. Renamed post_get property to request_body property for better readability.
 // 3. Added a comment in line 19 to indicate that v1Router is used for version 1 API requests.
+// 4. Fixed issues with duplicate usage of variables and missing async function.
