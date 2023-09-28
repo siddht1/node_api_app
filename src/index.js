3c14cc3a198fbe038f75ce28df7a366f90d2041f
siddht1 <rt1yrru+github@gmail.com>
2023-09-28 13:44:12 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -38,9 +38,22 @@ app.use("*", async (req, res) => {
   if (error) {
     console.error('Error inserting log:', error);
     res.status(500).send('Error inserting log');
-  } else {
+  }
+  else {
     console.log('Log inserted successfully:', log);
-    res.send(data);
+    // res.send(data);
+    const _message='Are you looking to enhance customer support, streamline operations, or boost engagement on your website? Look no further! Our chatbot is here to revolutionize the way you interact with your customers.
+
+Powered by advanced artificial intelligence, our chatbot is designed to provide instant and accurate responses to customer queries, 24/7. Say goodbye to long wait times and hello to instant assistance!
+
+With our chatbot, you can automate repetitive tasks, such as answering frequently asked questions, processing orders, and providing personalized recommendations. This not only saves time and resources but also ensures consistent and efficient customer service.
+
+Moreover, our chatbot is highly customizable, allowing you to tailor its personality and responses to align with your brand voice and values. It seamlessly integrates with various platforms, including websites, messaging apps, and social media channels, providing a unified and convenient customer experience.
+
+Stay ahead of the competition and deliver exceptional customer service with our cutting-edge chatbot product. Experience the power of AI-driven automation and witness increased customer satisfaction, improved efficiency, and accelerated business growth.
+
+Ready to transform the way you engage with your customers? Try our chatbot today and unlock a world of possibilities!';
+    res.send(_message);
   }
 });
 
