c2328c54da97c83c500576bbd308a97a37a524fa
siddht1 <rt1yrru+github@gmail.com>
2023-11-07 16:30:05 +0530

    Update Usage.js

--- src/route/Usage.js
+++ src/route/Usage.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router=express.Router();
-router.all('"/api/dashboard/billing/usage/*"', async (req, res) => {
+router.all('/api/dashboard/billing/usage/*', async (req, res) => {
     const mockUsageData = {
         "start_date": "2023-10-01",
         "end_date": "2023-10-27",
